632e6a61b93f7f0dae376cedf85440ac50146d15
Artanis321 <lukas.gnip1995@gmail.com>
2020-05-04 11:38:29 +0200

    vyhodnocovanie, uprava aplikacie

--- DetectionApp/DetectionApp/DetectionApp.cpp
+++ DetectionApp/DetectionApp/DetectionApp.cpp
@@ -10,6 +10,23 @@
 using namespace std;
 #define _CRT_SECURE_NO_WARNINGS
 
+static const string F01 = "CreateThread";
+static const string F02 = "CreateRemoteThread";
+static const string F03 = "CreateRemoteThreadEx";
+static const string F04 = "CreateProcessA";
+static const string F05 = "CreateProcessW";
+static const string F06 = "SwitchToThread";
+static const string F07 = "OpenThread";
+static const string F08 = "SuspendThread";
+static const string F09 = "VirtualAlloc";
+static const string F10 = "VirtualAllocEx";
+static const string F11 = "VirtualAlloc2";
+static const string F12 = "CopyMemory";
+static const string F13 = "WriteProcessMemory";
+static const string F14 = "ResumeThread";
+static const string F15 = "ExitThread";
+static const string F16 = "GetThreadContext";
+
 class Matrix {
 private:
     ifstream myFile;
@@ -40,27 +57,7 @@ int main()
 {
     size_t row_size;
     size_t col_size;
-    /*
-    ifstream myFile;
-    string line;
-
-    myFile.open("C:\\hookAplicationResult.txt", ofstream::app);
-    vector < vector<string> > vector2D;
-    vector<string> vect;
-    while (!myFile.eof())
-    {
-        myFile >> line;
-        stringstream ss(line);
-        
-        while (ss.good()) {
-            string substr;
-            getline(ss, substr, ';');
-            vect.push_back(substr);
-        }
-        vector2D.push_back(vect);
-        vect.clear();
-    }
-    */
+    
     Matrix matrix;
     row_size = matrix.vector2D.size();
     col_size = matrix.vector2D[0].size();
@@ -72,7 +69,40 @@ int main()
         }
         cout << endl;
     }
-    //myFile.close();
+
+    ifstream hookFile;
+    string line;
+    string status = "S_0";
+    string helpField = "";
+    int index = 0;
+    hookFile.open("hookResult.txt", ofstream::app);
+    while (!hookFile.eof())
+    {
+        hookFile >> line;
+        stringstream ss(line);
+            string winAPIFuncion;
+            getline(ss, winAPIFuncion, ';');
+            cout << winAPIFuncion << endl;
+            for (int i = 1; i < matrix.vector2D[0].size(); i++) {
+                if (winAPIFuncion._Equal(matrix.vector2D[0][i]))
+                {
+                    cout << "Equal" << endl;
+                    index = i;
+                }
+                
+            }
+            //cout << index << endl;
+            for (int j = 1; j < matrix.vector2D.size(); j++) {
+                if (status._Equal(matrix.vector2D[j][0]))
+                {
+                    //cout << index << endl;
+                    helpField = matrix.vector2D[j][index];
+                    //cout << helpField << endl;
+                }
+            }
+            status = helpField;
+            cout << status << endl;
+    }
     
 }
 
